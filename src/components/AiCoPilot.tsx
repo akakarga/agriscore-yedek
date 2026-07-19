b1b0f7381717f8224ae2f7927e5cee0038fc9b4f
Gökhan Demir <gokhan46pmh@gmail.com>
2026-07-19 21:34:09 +0300

    chore: deploy to vercel and fix bugs

--- src/components/AiCoPilot.tsx
+++ src/components/AiCoPilot.tsx
@@ -141,7 +141,7 @@ const AiCoPilot = () => {
   }
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end animate-fade-in print:hidden">
+    <div className="fixed bottom-6 right-6 z-[60] flex flex-col items-end animate-fade-in print:hidden">
       
       {/* Sohbet Kutusu */}
       {isOpen && (
